fb3eae1198df0f942a064eff9ffeb31780e66ea4
l4m-nt <l4m-nt@DESKTOP-36GDR7A>
2023-01-26 19:10:24 +0200

    Add project files.

---
+++ csvToEntity/Program.cs
@@ -0,0 +1,32 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csvToEntity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<WordTranslation> wordsList = new List<WordTranslation>();
+            Console.OutputEncoding = Encoding.UTF8;
+            var csv = new CsvReader(new StreamReader("../../1.csv", Encoding.UTF8), CultureInfo.InvariantCulture);
+            {
+                while(csv.Read())
+                {   
+                    if (csv[2] in wordsList.Contains())
+                    wordsList.Add(new WordTranslation(csv[2].ToString(), csv[3].ToString()));
+                    Console.WriteLine($"{csv[0]}, {csv[1]}, {csv[2]}, {csv[3]}");
+                }
+                //context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ csvToEntity/WordTranslation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace csvToEntity
+{
+    internal class WordTranslation
+    {
+        public WordTranslation() { }
+        public WordTranslation(string word, string translations)
+        {
+            Word = word;
+            Translations.Append(translations);
+        }
+        public WordTranslation(string word, List<string> translations)
+        {
+            Word = word;
+            Translations = translations;
+        }
+
+        public string Word { get; set; }
+
+        public List<string> Translations { get; set; }
+
+        public void AddWordTranslation(string translation)
+        {
+            Translations.Add(translation);
+        }
+
+        public override string ToString()
+        {
+            return $"{Word} - {string.Join(" ", Translations)}";
+        }
+    }
+}
